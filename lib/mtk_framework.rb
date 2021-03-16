7545a0cb4442915d96344fa35e8072eb8300b223
othmane el kesri <othmane@moneytrack.io>
2021-03-16 11:21:02 +0100

    refactor(lib): separate gem and core extensions into different files

--- lib/mtk_framework.rb
+++ lib/mtk_framework.rb
@@ -1,22 +1,22 @@
 # frozen_string_literal: true
 
+# External libs
 require 'active_interaction'
-puts ActiveInteraction::Base.class
 require 'tezos_client'
 require 'active_support/core_ext/hash/indifferent_access'
 
-require_relative 'active_interaction_ext/filters/abstract_tz_filter.rb'
-require_relative 'active_interaction_ext/filters/hash_filter.rb'
-require_relative 'active_interaction_ext/filters/tz_account_filter.rb'
-require_relative 'active_interaction_ext/filters/tz_contract_address_filter.rb'
-require_relative 'active_interaction_ext/filters/tz_operation_filter.rb'
-require_relative 'active_interaction_ext/filters/tz_public_key_filter.rb'
-require_relative 'active_interaction_ext/filters/tz_secret_key_filter.rb'
-require_relative 'active_interaction_ext/filters/tz_signature_filter.rb'
-require_relative 'active_interaction_ext/base.rb'
+# Gem extensions
+require_relative 'gem_extensions/active_interaction/filters/abstract_tz_filter.rb'
+require_relative 'gem_extensions/active_interaction/filters/hash_filter.rb'
+require_relative 'gem_extensions/active_interaction/filters/tz_account_filter.rb'
+require_relative 'gem_extensions/active_interaction/filters/tz_contract_address_filter.rb'
+require_relative 'gem_extensions/active_interaction/filters/tz_operation_filter.rb'
+require_relative 'gem_extensions/active_interaction/filters/tz_public_key_filter.rb'
+require_relative 'gem_extensions/active_interaction/filters/tz_secret_key_filter.rb'
+require_relative 'gem_extensions/active_interaction/filters/tz_signature_filter.rb'
+require_relative 'gem_extensions/active_interaction/base.rb'
 
-require_relative 'array/except.rb'
-
-require_relative 'class/virtual_method.rb'
-
-require_relative 'hash/rename_keys.rb'
+# Core extensions
+require_relative 'core_extensions/array/except.rb'
+require_relative 'core_extensions/class/virtual_method.rb'
+require_relative 'core_extensions/hash/rename_keys.rb'
